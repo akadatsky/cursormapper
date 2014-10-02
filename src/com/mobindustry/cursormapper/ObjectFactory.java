63d9d0a39aa446d8a534323edb666ed2d854653b
Den Drobiazko <den.ddro@gmail.com>
2014-10-02 11:07:33 +0300

    More code documented.

--- src/com/mobindustry/cursormapper/ObjectFactory.java
+++ src/com/mobindustry/cursormapper/ObjectFactory.java
@@ -1,5 +1,9 @@
 package com.mobindustry.cursormapper;
 
+/**
+ * Interface that will handle creation of new objects of type {@code <T>}
+ * @param <T> objects of this type will be created.
+ */
 public interface ObjectFactory<T> {
   T newInstance();
 }
